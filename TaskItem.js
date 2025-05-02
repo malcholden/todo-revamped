b3fefef900a38bf6dc1ad64bbdab120c883dcd4a
Malcolm Holden <holdenator55@gmail.com>
2025-05-02 11:40:39 -0400

    storage implemented to maintain tasks w refresh

--- TaskItem.js
+++ TaskItem.js
@@ -5,11 +5,10 @@ import doneSound from './assets/success.mp3'
 import delSound from './assets/no.mp3'
 import addedSound from './assets/yes.mp3';
 
+// This is the individual task item that is created when a new task is submitted. 
 export default function TaskItem({task, deleteTask, toggleCompleted}){
 
-    // let wonAudio = new Audio('./assets/won.wav');
-    // const [sound, setSound] = useState();
-
+    // plays sound
     async function playSound(soundFile) {
         const { sound } = await Audio.Sound.createAsync(soundFile);
         await sound.playAsync();
@@ -25,12 +24,14 @@ export default function TaskItem({task, deleteTask, toggleCompleted}){
     const [itemColor, setItemColor] = useState('#fcf4a4')
 
     
-
+      // when task has been completed: handles sound, color, function,etc.
     function done(){
         toggleCompleted(task.id);
         playSound(task.completed ? addedSound : doneSound);
         setItemColor(task.completed ? '#fcf4a4':'#f9bab9');
+        
     }
+    // when task has been deletd: handles sound, function, etc.
     function nvm(){
         deleteTask(task.id);
         playSound(delSound);
@@ -38,7 +39,7 @@ export default function TaskItem({task, deleteTask, toggleCompleted}){
     return(
         <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 5,
         marginBottom: 5,
-        padding: 10, flex: 2, backgroundColor: itemColor }}>
+        padding: 10, flex: 2, backgroundColor: task.completed ? '#f9bab9':'#fcf4a4' }}>
         <Pressable onPress={() => done() }>
         <Text style={{ fontSize: 18 }}>
           {task.completed ? '✅' : '⬜'}
